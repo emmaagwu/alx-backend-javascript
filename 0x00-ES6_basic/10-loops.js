d79fe3becbf40f193e40b74ef1783e6455b67194
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-06-24 13:12:53 +0100

    Add appendToEachArrayValue function to 10-loops.js

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,6 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (let idx = 0; idx < array.length; idx++) {
+    array[idx] = appendString + array[idx];
+  }
+  return array;
+}
