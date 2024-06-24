1d6efd9a91c02cf1c8ec053f4fc01d686f3723b9
Emmaagwu <emmanuelagwu89@gmail.com>
2024-06-24 22:19:50 +0000

    Refactor 10-loops file

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx = 0; idx < array.length; idx++) {
-    array[idx] = appendString + array[idx];
-  }
-  return array;
+  return array.map(value => appendString + value);
 }
