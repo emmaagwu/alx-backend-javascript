be49396cc553f2934093b225925fe8b4bbb5e83f
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-06-27 13:04:48 +0100

    Refactor HolbertonCourse constructor to validate input parameters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,28 @@
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    const errors = [];
+
+    if (typeof name !== 'string') {
+      errors.push('Name must be a string');
+    } else {
+      this._name = name;
+    }
+
+    if (typeof length !== 'number') {
+      errors.push('Length must be a number');
+    } else {
+      this._length = length;
+    }
+
+    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+      errors.push('Students must be an array of strings');
+    } else {
+      this._students = students;
+    }
+
+    if (errors.length > 0) {
+      throw new TypeError(errors.join(', '));
+    }
   }
 
   get name() {
@@ -42,3 +62,5 @@ export default class HolbertonCourse {
     }
   }
 }
+
+export default HolbertonCourse;
