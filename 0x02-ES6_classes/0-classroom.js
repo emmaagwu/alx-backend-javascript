8349bd2c0d616585c65e42eb12a51dd51ac5b13b
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-06-27 12:13:31 +0100

    Core: add new line and remove whitespace from file

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -4,8 +4,8 @@
  * Creates a new instance of the ClassRoom class.
  * @param {number} maxStudentsSize - The maximum size of the classroom.
  */
- 
-export default class ClassRoom {  
+
+export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
