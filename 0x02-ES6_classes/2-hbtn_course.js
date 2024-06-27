e0e0d7343982cb42accb27dbb4a5a8d7b0354be4
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-06-27 12:25:33 +0100

    Fix type check in set length method

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -26,7 +26,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof length === 'number') {
+    if (typeof length === Number) {
       this._length = length;
     } else {
       throw new TypeError('Length must be a number');
