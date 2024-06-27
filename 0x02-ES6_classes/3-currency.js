e6988b441e3194185e9af922b6d5384bc6313e8f
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-06-27 13:41:59 +0100

    Add Currency class with code and name properties and displayFullCurrency method

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,3 +1,24 @@
+/**
+ * Class representing a currency.
+ *
+ * @class
+ * @classdesc This class represents a currency with a code and name.
+ *
+ * @example
+ * const currency = new Currency('USD', 'United States Dollar');
+ * console.log(currency.displayFullCurrency()); // Outputs: "United States Dollar (USD)"
+ *
+ * @property {string} code - The currency code (e.g., 'USD').
+ * @property {string} name - The name of the currency (e.g., 'United States Dollar').
+ *
+ * @method get code - Gets the currency code.
+ * @method set code - Sets the currency code.
+ * @method get name - Gets the name of the currency.
+ * @method set name - Sets the name of the currency.
+ * @method displayFullCurrency - Returns a string representation of the
+ *    currency in the format "name (code)".
+ */
+
 export default class Currency {
   constructor(code, name) {
     this._code = code;
