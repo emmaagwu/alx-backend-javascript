cfa33627a3f0b39109b2dcf2fbf7d45af41b003e
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-06-27 12:07:51 +0100

    Add ClassRoom class definition

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a classroom.
+ *
+ * Creates a new instance of the ClassRoom class.
+ * @param {number} maxStudentsSize - The maximum size of the classroom.
+ */
+ 
+export default class ClassRoom {  
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
