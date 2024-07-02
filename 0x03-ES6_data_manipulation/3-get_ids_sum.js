3f7a1b124a36d771b5899a7b8c4f95416d25af03
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-07-02 12:51:36 +0100

    Refactor getStudentIdsSum function to calculate the sum of student IDs

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,8 @@
+/**
+ * Calculates the sum of student IDs.
+ * @param {Array} student - The array of student objects.
+ * @returns {number} The sum of student IDs.
+ */
 export default function getStudentIdsSum(student) {
-  if (!Array.isArray(student)) {
-    return 0;
-  }
   return student.reduce((sum, student) => sum + student.id, 0);
 }
